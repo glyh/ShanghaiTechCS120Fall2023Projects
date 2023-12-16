d6350da31856ffe5f17be1ba0769526c85f8e4b7
glyh <lyhokia@gmail.com>
2023-12-16 23:37:49 +0800

    checkpoint for physic and ringbuf

--- proj2/AMC/src/main/kotlin/cs120/physics/Physics.kt
+++ proj2/AMC/src/main/kotlin/cs120/physics/Physics.kt
@@ -1,4 +1,5 @@
 package cs120.physics
+import cs120.RingBuffer.RingBuffer
 
 import kotlin.math.sin
 import kotlinx.coroutines.channels.*
@@ -6,12 +7,14 @@ import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.DurationUnit
 import kotlinx.coroutines.*
 import java.nio.ByteBuffer
-import javax.sound.sampled.AudioFormat
-import javax.sound.sampled.AudioSystem
-import javax.sound.sampled.DataLine
-import javax.sound.sampled.SourceDataLine
+import javax.sound.sampled.*
 import kotlin.time.Duration
+import kotlin.math.ceil
+import kotlin.math.max
+import com.github.psambit9791.jdsp.transform.FastFourier
+import kotlin.math.abs
 
+val delay_duration = 5.milliseconds
 const val fs = 44100.0
 suspend fun sleep(duration: Duration, start: Channel<Boolean>, end: Channel<Boolean>) {
     start.receive()
@@ -19,26 +22,25 @@ suspend fun sleep(duration: Duration, start: Channel<Boolean>, end: Channel<Bool
     end.send(true)
 }
 
-val delay_duration = 5.milliseconds
+val preambleDuration = 800.milliseconds
+val preambleWidth = (preambleDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
+const val preambleStartFreq = 1000.0 // hz
+const val preambleEndFreq = 5000.0 // hz
+val chirpRate = (preambleEndFreq - preambleStartFreq) / preambleDuration.toDouble(DurationUnit.SECONDS) // hz per second
+
+val format = AudioFormat(fs.toFloat(), Short.SIZE_BYTES * 8, 1, true, true)
+val info = DataLine.Info(SourceDataLine::class.java, format)
+
 suspend fun preamble(start: Channel<Boolean>, end: Channel<Boolean>) {
-    val preambleDuration = 800.milliseconds
-    val preambleStartFreq = 1000.0 // hz
-    val preambleEndFreq = 5000.0 // hz
+    if (!AudioSystem.isLineSupported(info)) {
+        throw IllegalArgumentException("Audio line not supported")
+    }
 
-    val chirpRate = (preambleEndFreq - preambleStartFreq) / preambleDuration.toDouble(DurationUnit.SECONDS) // hz per second
-    val frameCount = (preambleDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
-
-    val data = (0 until frameCount).map {
+    val data = (0 until preambleWidth).map {
         val f = sin(2 * Math.PI * (
                 chirpRate / 2.0 / fs / fs * it * it +
                         preambleStartFreq / fs * it))
         (f * Short.MAX_VALUE).toInt().toShort()
-    }
-
-    val format = AudioFormat(fs.toFloat(), Short.SIZE_BYTES * 8, 1, true, true)
-    val info = DataLine.Info(SourceDataLine::class.java, format)
-    if (!AudioSystem.isLineSupported(info)) {
-        throw IllegalArgumentException("Audio line not supported")
     }
 
     start.receive()
@@ -70,11 +72,11 @@ suspend fun preamble(start: Channel<Boolean>, end: Channel<Boolean>) {
     end.send(true)
 }
 
-suspend fun sendData(data: List<Byte>, start: Channel<Boolean>, end: Channel<Boolean>) {
-    val modDuration = 300.milliseconds
+val modDuration = 300.milliseconds
+val modWidth = ceil(modDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
 
-    val format = AudioFormat(fs.toFloat(), Short.SIZE_BYTES * 8, 1, true, true)
-    val info = DataLine.Info(SourceDataLine::class.java, format)
+suspend fun sendData(data: List<Byte>, start: Channel<Boolean>, end: Channel<Boolean>) {
+
     if (!AudioSystem.isLineSupported(info)) {
         throw IllegalArgumentException("Audio line not supported")
     }
@@ -94,12 +96,11 @@ suspend fun sendData(data: List<Byte>, start: Channel<Boolean>, end: Channel<Boo
     val cBuf = ByteBuffer.allocate(line.bufferSize)
 
     var offset = 0
-    val samplePerSym = (modDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
-    val totalSample = data.size * samplePerSym
+    val totalSample = data.size * modWidth
     val totalSampleBytes = totalSample / 2 * 3
     while (offset < totalSampleBytes) {
         val samplesThisPass = line.available() / Short.SIZE_BYTES
-        val symThisPass = samplesThisPass / samplePerSym
+        val symThisPass = samplesThisPass / modWidth
         val bytesThisPass = symThisPass / 2 * 3
         for(i in 0 until bytesThisPass step 3) {
             val b1 = data[offset+i].toInt()
@@ -109,10 +110,10 @@ suspend fun sendData(data: List<Byte>, start: Channel<Boolean>, end: Channel<Boo
             val short1 = ((b1 shl 4) and (b2 shr 4)).toShort()
             val short2 = (((b2 and 0b1111) shl 8) and b3).toShort()
 
-            for(j in 0..samplePerSym) {
+            for(j in 0..modWidth) {
                 cBuf.putShort(short1)
             }
-            for(j in 0..samplePerSym) {
+            for(j in 0..modWidth) {
                 cBuf.putShort(short2)
             }
         }
@@ -128,8 +129,9 @@ suspend fun sendData(data: List<Byte>, start: Channel<Boolean>, end: Channel<Boo
     end.send(true)
 }
 
+val sleepDuration = 500.milliseconds
+
 suspend fun send(a: List<Byte>) {
-    val sleepDuration = 500.milliseconds
     val startSig = Channel<Boolean>()
     val afterSleep = Channel<Boolean>()
     val afterPreamble = Channel<Boolean>()
@@ -144,12 +146,108 @@ suspend fun send(a: List<Byte>) {
         launch {
             preamble(afterSleep, afterPreamble)
         }
-        launch {
-            sendData(a, afterPreamble, afterData)
+        sendData(a, afterPreamble, afterData)
+    }
+}
+
+fun maxEnergyFrequency(signalShort: Array<Short>): Double {
+    val signal = signalShort.map { it.toDouble() }
+    val fft = FastFourier(signal.toDoubleArray())
+    fft.transform()
+    val energies = fft.getMagnitude(true).toTypedArray()
+    val frequencies = fft.getFFTFreq(fs.toInt(), true)
+    return frequencies[argMax(energies)]
+}
+
+fun <T: Comparable<T>> argMax(a: Array<T>): Int {
+    var ans = 0
+    var maximum = a[0]
+    for (i in 1 until a.size) {
+        if (a[i] > maximum) {
+            maximum = a[i]
+            ans = i
         }
+    }
+    return ans
+}
+
+val sliceDuration = 40.milliseconds
+val sliceWidth = ceil(sliceDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
+val sliceInnerDuration = 10.milliseconds
+val sliceInnerWidth = ceil(sliceInnerDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
+
+fun tryDetectPreamble(buf: RingBuffer<Short>): Pair<Boolean, Int> {
+    val sliceNum = 10
+
+    val cutoffVariancePreamble = 0.5
+
+    var variance = 0.0
+    val toAnalyze = Array<Short>(sliceWidth - 2 * sliceInnerWidth) { 0 }
+    var freqShift = 0.0
+    for(i in 0 until sliceNum) {
+        buf.copyStrideRight(
+            i * sliceWidth + sliceInnerWidth,
+            sliceWidth - 2 * sliceInnerWidth,
+            toAnalyze)
+
+        val maxEnergyFreq = maxEnergyFrequency(toAnalyze)
+        val avgFreq = preambleEndFreq - (i + 0.5) * sliceDuration.toDouble(DurationUnit.SECONDS) * chirpRate
+        if (i == 0 && abs(avgFreq - maxEnergyFreq) < sliceDuration.toDouble(DurationUnit.SECONDS) * chirpRate) {
+            freqShift = maxEnergyFreq - avgFreq
+        }
+        val delta = (avgFreq - maxEnergyFreq + freqShift) / avgFreq
+        variance += delta * delta
+    }
+    return if (variance < cutoffVariancePreamble) {
+        Pair(true, ceil(freqShift * fs).toInt())
+    } else {
+        Pair(false, 0)
     }
 }
 
 fun receive(): Array<Byte> {
-    return arrayOf()
+    if (!AudioSystem.isLineSupported(info)) {
+        throw IllegalArgumentException("Audio line not supported")
+    }
+    val line = AudioSystem.getLine(info) as TargetDataLine
+    line.open(format)
+
+    // TODO: fix below
+    val singleSymbolSamplesRequired = ceil(modDuration.toDouble(DurationUnit.SECONDS) * fs).toInt()
+
+    // we may look back for 2 symbols
+    val bufSize = max(
+        preambleWidth,
+        singleSymbolSamplesRequired * 2)
+    // reserve some space for error
+    val buf = RingBuffer<Short>(bufSize * 3)
+
+    line.start()
+    val stream = AudioInputStream(line)
+
+    val bytesPerSlice = sliceWidth * Short.SIZE_BYTES
+
+    val sliceBuf = ByteBuffer.allocate(bytesPerSlice)
+    var frameCountAll = 0
+
+    var isIdle = true
+
+    while (true) {
+        if (isIdle) {
+            stream.read(sliceBuf.array(), 0, bytesPerSlice)
+            for(i in 0 until bytesPerSlice step Short.SIZE_BYTES) {
+                buf.put(sliceBuf.getShort(i))
+                frameCountAll += 1
+            }
+            if (frameCountAll >= preambleWidth) {
+                val (ok, frameCountNew) = tryDetectPreamble(buf)
+                if(ok) {
+                    isIdle = false
+                    frameCountAll = frameCountNew
+                }
+            }
+        } else {
+            // TODO
+        }
+    }
 }
